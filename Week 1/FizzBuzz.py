f19d057a2d2aae499d08050ad3c134928a1457af
Tibebu12aa <99277608+Tibebu12aa@users.noreply.github.com>
2022-08-15 10:04:41 +0300

    Update FizzBuzz.py

--- Week 1/FizzBuzz.py
+++ Week 1/FizzBuzz.py
@@ -1,12 +1,15 @@
-n=int(input("Enter any integer: "))
+
+n =int(input())
 list=[]
-for i in range(1,n+1):
-    if i%3==0 and i%5==0:
+i=1
+while i <= n:
+    if i%5 ==0 and i%3:
         list.append("FizzBuzz")
     elif i%3==0:
         list.append("Fizz")
     elif i%5==0:
         list.append("Buzz")
     else:
-        list.append(str(i))
+        list.append(i)
+    i+=1 
 print(list)
