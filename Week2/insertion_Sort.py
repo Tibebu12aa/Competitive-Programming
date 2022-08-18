979b8971815dbd44b8a1b1a344d2179e6de754b3
Tibebu Kassa <tibebu12aa@gmail.com>
2022-08-18 10:48:33 +0300

    update insersion_sort

--- Week2/insertion_Sort.py
+++ Week2/insertion_Sort.py
@@ -1,21 +1,33 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'insertionSort1' function below.
 #
 # The function accepts following parameters:
 #  1. INTEGER n
 #  2. INTEGER_ARRAY arr
 #
 
-def insertionSort1(arr):
-    for i in range(1,len(arr)):
-        j=i
-        while arr[j-1]>arr[j] and j>0:
-            arr[j],arr[j-1] = arr[j-1], arr[j]
-            print(arr)
-            j-= 1
-
+def insertionSort1(n, arr):
+    lastelement=arr[-1]
+    i= n-1
+    while i>0 and arr[i-1] >lastelement:
+        arr[i]=arr[i-1]
+        print(*arr)
+        i -= 1
+    arr[i]=lastelement
+    print(*arr)
+            
 if __name__ == '__main__':
     n = int(input().strip())
 
     arr = list(map(int, input().rstrip().split()))
 
-    insertionSort1(arr)
+    insertionSort1(n, arr)
