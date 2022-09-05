7133d81cedde909c57217dd9f81991f3cdef22b7
Tibebu Kassa <tibebu12aa@gmail.com>
2022-09-05 10:56:51 +0300

    first_problem_with_mentor

--- Week 3/Valid_parentheses.py
+++ Week 3/Valid_parentheses.py
@@ -1,19 +1,28 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        #first i crated two lists containg both open and close parentheses with each parenthesis in openlist
+        # have the same index in the closed list
         open_list = ["{","[","("]
         closed_list = ["}","]",")"]
-        stack = []
+        #then i created an empty stack 
+        My_stack = []
+        #now for every element in the list s this programm will iterate the valdity by appending and poping elments in to My_stack
         for i in s:
+            #in the loop first i is from the opend list it will append(push) it to the empty stack  
             if i in open_list:
-                stack.append(i)
+                My_stack.append(i)
+            ### else if if i is from the closed list it will check if its the same kinda of parenthesis by 
+            # by setting the index of the element in the closed list to a variable named checkindex
+            # and then check if its the same as the last stack element
             elif i in closed_list:
-                pos = closed_list.index(i)
-                
-                if((len(stack)>0) and open_list[pos] == stack[len(stack)-1]):
-                    stack.pop()
+                checkindex=closed_list.index(i)
+                if len(My_stack)>0 and open_list[checkindex]==My_stack[len(My_stack)-1]:
+                    #then if it is it will pop the last element in the stack making it an empty stack
+                    My_stack.pop()
                 else:
                     return False
-        if len(stack)==0:
+        #finallly if the stack is not empty then it doesn't have a valid parentheses
+        if len(My_stack)==0:
             return True
         else:
             return False
