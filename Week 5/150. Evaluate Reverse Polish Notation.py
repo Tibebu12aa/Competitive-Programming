8a0dc6015b600154ad48998ff8f19f7e97406c9b
Tibebu12aa <99277608+Tibebu12aa@users.noreply.github.com>
2022-09-05 23:06:42 +0300

    Update 150. Evaluate Reverse Polish Notation.py

--- Week 5/150. Evaluate Reverse Polish Notation.py
+++ Week 5/150. Evaluate Reverse Polish Notation.py
@@ -5,7 +5,7 @@ class Solution:
         stack = []      
         for i in tokens:
             """
-            else if the element is not an operator then for each operator we run the operation for the two               elements in the stack and store the result as the 
+            else if the element is not an operator then for each operator we run the operation for the last two elements in the stack and store the result as the 
             last element by removing the previous last element using pop
             """ 
             if i == "+":
