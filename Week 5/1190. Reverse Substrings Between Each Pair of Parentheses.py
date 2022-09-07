8a126dda47135b0a852bef1b331af2d73c3afc2e
Tibebu Kassa <tibebu12aa@gmail.com>
2022-09-07 11:04:38 +0300

    E

--- Week 5/1190. Reverse Substrings Between Each Pair of Parentheses.py
+++ Week 5/1190. Reverse Substrings Between Each Pair of Parentheses.py
@@ -1,5 +1,15 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
+        """
+        for this operation i choose to start from the inner loop so 
+        first we create an empty stack
+        now for every charachter in string s if the charachter is not ")" we simply append it to the stack
+        else if the charcher is ")" we pop the all the elemnts behind it until we reach "(" and inorder to 
+        reverse it we append them to a tmp variable  
+        after that we now remove the "(" charcter which is now the last element of the stack
+        now we append all the elemnts in tmp to the stack 
+        finaly we print the stack as a single string by usig the join method
+        """
         stack = []
         for i in s:
             if i == ')':
@@ -11,3 +21,23 @@ class Solution:
             else:
                 stack.append(i)
         return "".join(stack)
+            
+        """
+        test case
+         #  ["(ed(et(oc))el)"]
+        # stack=["(ed(et(oc"]
+        # tmp=""
+        # temp="co"
+        # stack=["(ed(et]
+        # stack=["(ed(etco]
+        # tmp=""
+        # tmp="octe"
+        # stack=["(ed]
+        # stack=["(edocte]
+        # stack=["(edocteel]
+        # temp=""
+        # temp="leetcode"
+        # stack=[""]
+        # stack=["leetcode"]
+        return = leetcode
+        """
