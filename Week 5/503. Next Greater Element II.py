b8449b17a52fab807f91e50020b249ad33a802ab
Tibebu Kassa <tibebu12aa@gmail.com>
2022-09-08 14:37:25 +0300

    H

--- Week 5/503. Next Greater Element II.py
+++ Week 5/503. Next Greater Element II.py
@@ -1,11 +1,13 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        size = len(nums)
-        nums+=nums
-        final = [-1] * size
+        n = len(nums)
+        ans = n * [-1]
+        nums += nums
+        
         stack = []
-        for i in list(range(size))*2:
-            while stack and (nums[stack[-1]] < nums[i]):
-                final[stack.pop()] = nums[i]
-            stack.append(i)
-        return final
+        for i, num in enumerate(nums):
+            while stack and stack[-1][1] < num:
+                ans[stack.pop()[0]] = num
+            if i < n: stack.append((i, num))
+        
+        return ans
