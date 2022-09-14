d79acee0215a18e2d3c3b56fc3467175cbe8533c
Tibebu Kassa <tibebu12aa@gmail.com>
2022-09-14 16:32:57 +0300

    M

--- Week 6/274. H-Index.py
+++ Week 6/274. H-Index.py
@@ -1,12 +1,9 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        n = len(citations)
-        papers = [0] * (n + 1)  # papers[i] is the number of papers with i citations.
-        for c in citations:
-            papers[min(n, c)] += 1  # All papers with citations larger than n is count as n.
-        i = n
-        s = papers[n]  # sum of papers with citations >= i
-        while i > s:
-            i -= 1
-            s += papers[i]
-        return i
+        citations.sort()
+        i,n = 1,len(citations)
+        while i<=n:
+            if citations[n-i]<i: 
+                break
+            i+=1
+        return i-1
