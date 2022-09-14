5f100f932a6a9d3b6b0ba4494c389bd80875b348
Tibebu Kassa <tibebu12aa@gmail.com>
2022-09-14 16:58:42 +0300

    E

--- Week 6/274. H-Index.py
+++ Week 6/274. H-Index.py
@@ -1,9 +1,13 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+        # first we sort the citations list
         citations.sort()
-        i,n = 1,len(citations)
-        while i<=n:
-            if citations[n-i]<i: 
+        # then we initialize both h and n
+        h,n= 1, len(citations)
+        # so now we start a loop that continues to upgrade h to the end of the list
+        while h<=n:
+            # now if h is less than the n-h index the h-1 is the h-index
+            if citations[n-h]<h:
                 break
-            i+=1
-        return i-1
+            h+=1
+        return h-1
