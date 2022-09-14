4a6af10f1510e285540db1334dd331ae4ccf017d
Tibebu Kassa <tibebu12aa@gmail.com>
2022-09-14 11:07:54 +0300

    M

--- Week 6/946. Validate Stack Sequences.py
+++ Week 6/946. Validate Stack Sequences.py
@@ -1,11 +1,14 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
         stack=[]
-        i=0
-        for num in pushed:
-            stack.append(num) #we are pushing the number to the stack
-            while  len(stack) >0 and stack[len(stack)-1] == popped[i] :
-                #if the last element of the stack is equal to the popped element
+        j=0
+        for i in pushed:
+            stack.append(i)
+            while stack and stack[-1]==popped[j]:
                 stack.pop()
-                i+=1 #we are incrementing i
-        return True if len(stack) ==0 else False
+                j+=1
+        if len(stack)==0:
+            return True
+        else:
+            return False
+       
