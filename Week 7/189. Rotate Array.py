c2e6cf32381bc2fc5e49e64c6bd9fdae54dcc44c
Tibebu Kassa <tibebu12aa@gmail.com>
2022-09-26 14:23:44 +0300

    M

--- Week 7/189. Rotate Array.py
+++ Week 7/189. Rotate Array.py
@@ -3,5 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(k):
-            nums.insert(0,nums.pop())
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums) #if k == len(nums) i.e no rotation
+        
+        #reverse array
+        l, r = 0, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+         
+        #reverse first k elements
+        l, r = 0, k - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+          
+        #reverse remmaining elements
+        l, r = k, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
