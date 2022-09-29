5c459c39283106be4ccd3b116157ad78ce9eb167
Tibebu Kassa <tibebu12aa@gmail.com>
2022-09-29 21:36:26 +0300

    M

--- Week 8/560. Subarray Sum Equals K.py
+++ Week 8/560. Subarray Sum Equals K.py
@@ -1,19 +1,18 @@
-class Solution:
-	def subarraySum(self, nums: List[int], k: int) -> int:
 
-		ans=0
-		prefsum=0
-		d={0:1}
+nums = [1,2,3], 
+k = 3
+ans=0
+prefsum=0
+d={0:1}
 
-		for num in nums:
-			prefsum = prefsum + num
+for num in nums:
+	prefsum = prefsum + int(num)
 
-			if prefsum-k in d:
-				ans = ans + d[prefsum-k]
+	if prefsum-k in d:
+		ans = ans + d[prefsum-k]
 
-			if prefsum not in d:
-				d[prefsum] = 1
-			else:
-				d[prefsum] = d[prefsum]+1
-
-		return ans
+	if prefsum not in d:
+		d[prefsum] = 1
+	else:
+		d[prefsum] = d[prefsum]+1
+print(d)
