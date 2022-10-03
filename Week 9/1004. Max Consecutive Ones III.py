ff5b591b256dd70979529725d658a54f5287ff02
Tibebu Kassa <tibebu12aa@gmail.com>
2022-10-03 11:52:21 +0300

    H

---
+++ Week 9/1004. Max Consecutive Ones III.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return 0
+        
+        zero_idx = {i:-1 for i in range(-k, 1)}
+        print(zero_idx)
+        ans, zeros = 0, 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                zeros += 1
+                zero_idx[zeros] = i
+               
+            ans = max(ans, i - zero_idx[zeros - k])
+        return ans   
